169e3f9c202a47970d6f88e7410db8d25d1b7dd8
PauloVitorOliv <al.paulo.oliveira@impatech.org.br>
2025-07-18 01:36:08 -0300

    updt minimal

--- Alglin/LU-Decomposition.py
+++ Alglin/LU-Decomposition.py
@@ -1,57 +1,56 @@
 import numpy as np
-import scipy as sci
 
-# Comparação com tolerância a erro de ponto flutuante
-eq = np.frompyfunc(lambda a,b: np.abs(a-b) < 1e-12, 2, 1)
+compare = np.frompyfunc(lambda a,b: np.abs(a-b) < 1e-12, 2, 1)
 
-def LU_Decomposition(A): #Numpy
-	n = len(A); m = len(A[0]) #A = nxm
-	nl = 0 #Próxima linha livre
-	index = np.zeros(n,dtype=int) #Índice das linhas, inicial = posição 
-	for i in range(n):
-		index[i] = i
-  
-	P = np.eye(n); L = np.eye(n); U = A.copy()
-	ops = []
+def LU_Decomposition(A):
+    n = len(A); m = len(A[0]); nL = 0
+    index = np.arange(n,dtype=int)
+    where = np.arange(n,dtype=int)
 
-	#Início da iteração
-	for i in range(m):
-		if np.all(eq(U[nl:,i],U[nl:,i]*0)):
-			continue
-		idx = np.argmax(np.abs(U[nl:,i]))+nl
-  
-		#Troca as linhas do máximo e de cima
-		U[[nl,idx]] = U[[idx, nl]]
-		P[[nl,idx]] = P[[idx, nl]]
-		index[nl],index[idx] = index[idx],index[nl]
+    P = np.eye(n)
+    L = np.eye(n)
+    U = A.copy()
+    operations = []
+    
+    for nC in range(m):
+        if np.all(compare(U[nL:,nC],0)):
+            continue
+        l = np.argmax(np.abs(U[nL:,nC]))+nL
+        
+        if nL != l:
+            U[[nL,l]] = U[[l, nL]]
+            P[[nL,l]] = P[[l, nL]]
+            index[nL],index[l] = index[l],index[nL]
+            where[index[nL]],where[index[l]] = where[index[l]],where[index[nL]]
+            
+        alpha = U[nL+1:,nC] / U[nL,nC]
+        for i,x in enumerate(alpha,start=nL+1):
+            operations.append([index[i],x,index[nL]])
+        
+        for i in range(nL+1,n):
+            U[i] -= alpha[i-nL-1]*U[nL]
 
-		#Zera as outras linhas
-		factor = U[nl+1:,i].reshape(n-nl-1,1) / U[nl,i]
-		for ii,x in enumerate(factor,start=nl+1):
-			ops.append([index[ii],index[nl],x.item()])
-		subtractors = np.repeat(U[nl:nl+1],n-nl-1,axis=0) * factor
-		U[nl+1:] -= subtractors
-		nl += 1
-		if (nl == n):
-			break
+        nL += 1
+        if (nL == n):
+            break
+    
+    for op in operations:
+        i = where[op[0]]
+        j = where[op[2]]
+        L[:, j] += op[1] * L[:, i]
 
-	#Construir vetor que informa qual linha está a linha com cada índice
-	where = np.zeros(shape=[n,1],dtype=int)
-	for i in range(n):
-		where[index[i]] = i
+    return P,L,U
 
-	#Construir matriz L
-	for op in ops:
-		Li = np.eye(n)
-		Li[where[op[0]].item(), where[op[1]].item()] = op[2]
-		L @= Li
-  
-	return P, L, U
-  
-A = np.array([
-    [5.2, 3, -100],
-    [161, 2.3, 69],
-    [1, -4, 133.1]
+B = np.array([
+    [1,2,4,5,6],
+    [-3,5,0,11,4],
+    [1,-41,1.1,-5,.5],
+    [-3,-3,-9,99,0],
+    [0,13,15,0,13]
 ],dtype=np.float64)
 
-P, L, U = LU_Decomposition(A)
+P,L,U = LU_Decomposition(B)
+
+print(P)
+print(L)
+print(U)
